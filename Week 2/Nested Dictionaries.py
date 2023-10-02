ef7f6f80f3501a6875c645148c3b35499a037b71
Muyinjon "M" Turobov <73412508+Muyinjon@users.noreply.github.com>
2023-10-01 22:22:05 -0400

    Update Nested Dictionaries.py

--- Week 2/Nested Dictionaries.py
+++ Week 2/Nested Dictionaries.py
@@ -3,3 +3,28 @@ Accessing Nested Elements: Fetch the age of a specific student.
 Updating Nested Dictionary: Update the grade of a student.
 Looping through Nested Dictionaries: Print out each student's name along with their age and grade.
 '''
+def line():
+    print('______________________________________________________________________________')
+students = {
+    'jonny': {'age': 19, 'grade': 'A'},
+    'ricky': {'age': 25, 'grade': 'B'},
+    'cowboy': {'age': 24, 'grade': 'C'},
+    'yana': {'age': 16, 'grade': 'A'}
+}
+print(students)
+
+line()
+a= students['jonny']
+print(a)
+
+line()
+
+b = students['ricky']['grade'] = 'A+'
+print(b)
+print(students)
+line()
+
+for student,studentdetails in students.items():
+    print(f"Student Name: {student}")
+    print(f"Age: {studentdetails['age']}")
+    print(f"Grade: {studentdetails['grade']}")
